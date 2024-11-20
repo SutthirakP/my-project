bf4da635e83cf3bf48b0a9f4a4def6724c0888f5
SutthirakP <6630613041@email.psu.ac.th>
2024-11-20 07:03:23 +0700

    ✅ Post requst to create user

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -1,16 +1,26 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// api/admin/users/route.ts
+import { NextResponse } from 'next/server';
 import prisma from '@/app/utils/db';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const users = await prisma.user.findMany();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching users' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+// Handle GET request to fetch all users
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json({ error: 'Error fetching users' }, { status: 500 });
+  }
+}
+
+// Handle POST request to create a new user
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    const newUser = await prisma.user.create({
+      data,
+    });
+    return NextResponse.json(newUser, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Error creating user' }, { status: 500 });
   }
 }
