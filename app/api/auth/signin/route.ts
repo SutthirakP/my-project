e1604233d10bd41d390d211ce166728df58d2023
SutthirakP <6630613041@email.psu.ac.th>
2024-11-29 03:01:02 +0700

    🔒️ include check username | password

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -7,39 +7,60 @@ export async function POST(request: Request) {
   try {
     const { username, password } = await request.json();
 
-    // ตรวจสอบว่า username กับ password มีข้อมูลครบถ้วนหรือไม่
+    // ตรวจสอบว่า username และ password มีค่าหรือไม่
     if (!username || !password) {
-      return NextResponse.json({ message: 'Username and password are required' }, { status: 400 });
+      return NextResponse.json(
+        { message: 'Username and password are required' },
+        { status: 400 }
+      );
     }
 
-    // ค้นหาผู้ใช้ในฐานข้อมูล
+    // ค้นหาผู้ใช้ในฐานข้อมูล โดยดึงเฉพาะฟิลด์ที่จำเป็น
     const user = await prisma.user.findUnique({
       where: { username },
+      select: {
+        id: true,
+        username: true,
+        password: true,
+        role: true,
+      },
     });
 
-    if (user && await bcrypt.compare(password, user.password)) {
-      // สร้าง JWT token
-      if (!process.env.JWT_SECRET) {
-        throw new Error('JWT_SECRET is not defined in the environment variables');
-      }
-
-      const token = jwt.sign(
-        { userId: user.id, role: user.role },
-        process.env.JWT_SECRET,
-        { expiresIn: '1h' } // กำหนดระยะเวลาของ token
+    // ตรวจสอบว่า username และ password ตรงกันหรือไม่
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return NextResponse.json(
+        { message: 'Invalid username or password' },
+        { status: 401 }
       );
-
-      return NextResponse.json({ token, username: user.username, role: user.role });
-    } else {
-      return NextResponse.json({ message: 'Invalid username or password' }, { status: 401 });
     }
+
+    // ตรวจสอบตัวแปร JWT_SECRET
+    if (!process.env.JWT_SECRET) {
+      throw new Error('JWT_SECRET is not defined in the environment variables');
+    }
+
+    // สร้าง JWT token
+    const token = jwt.sign(
+      { userId: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' } // กำหนดอายุของ token
+    );
+
+    // ส่งผลลัพธ์กลับ
+    return NextResponse.json({
+      token,
+      username: user.username,
+      role: user.role,
+    });
   } catch (error) {
     console.error('Error during authentication:', error);
-  
-    if (error instanceof Error) {
-      return NextResponse.json({ message: 'Something went wrong', error: error.message }, { status: 500 });
-    } else {
-      return NextResponse.json({ message: 'Something went wrong', error: 'Unknown error' }, { status: 500 });
-    }
+
+    return NextResponse.json(
+      {
+        message: 'Something went wrong',
+        error: error instanceof Error ? error.message : 'Unknown error',
+      },
+      { status: 500 }
+    );
   }
-}  
+}
