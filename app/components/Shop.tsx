6d55e5d091791b173bccbcf39f0147d9bc45d0b3
SutthirakP <6630613041@email.psu.ac.th>
2024-12-01 19:33:05 +0700

    🔥  delete h2 shop

--- app/components/Shop.tsx
+++ app/components/Shop.tsx
@@ -177,7 +177,6 @@ export default function Shop() {
 
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-4">Shop</h1>
 
       {/* Search Bar and Sorting */}
       <div className="flex justify-between items-center mb-4">
